73d598757d68c789ca3030a742c81d371c6edddd
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2020-09-15 10:34:21 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import $ from "jquery";
+import autocomplete from 'jquery-ui/ui/widgets/autocomplete';
+
+$(function() {
+
+    $("#search").autocomplete({
+            source: function(request, response) {
+                $.ajax({
+                        url: "http://api.tvmaze.com/singlesearch/shows?q=" + request.term + "&embed=episodes",
+                        dataType: "json",
+                        data: {
+                            q: request.term
+                        },
+
+
+                        success: function(data) {
+
+                            var allEposodes = data._embedded.episodes;
+
+                            function test() {
+                                for (var key in allEposodes) {
+                                    if (allEposodes.hasOwnProperty(key)) {
+                                        return {
+                                            image: allEposodes[key].image.medium,
+                                        }
+                                    }
+                                }
+                            }
+
+                            var results = $.map(allEposodes, function(item)
+
+                                return {
+                                    label: item.name,
+                                    url: item.url,
+                                    value: data.name,
+                                    desc: item.summary,
+                                    season: item.season,
+                                    image: item.image
+
+
+
+                                }
+                            });
+
+                        response(results);
+
+                    }
+                });
+        },
+
+
+        minLength: 3,
+        select: function(event, ui) {
+            var description = JSON.stringify(ui.item.desc);
+
+            var stripedHtmldesc = description.replace(/<[^>]+>/g, '');
+
+            $("#project-description").text(stripedHtmldesc);
+
+
+            return false;
+
+        }
+    })
+.autocomplete("instance")._renderItem = function(ul, item) {
+    return $("<li>")
+        .append("<img src='" + item.image + "' />" + item.label + "<br><span>Season: </span>" + item.season + "<br><br>")
+        .appendTo(ul);
+};
+});
